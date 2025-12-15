13a8ac3c7531fd618cdbef83a925b7dbec879877
rohitmanjhi96 <rohitmanjhibtc.com>
2025-12-15 12:11:57 +0530

    stack program implement

---
+++ stack.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+    
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.push(6)
+stack.push(7)
+stack.push(8)
+stack.push(9)
+stack.push(10)
+print(stack.size())
+stack.pop()
+stack.pop()
+stack.pop()
+print(stack)
