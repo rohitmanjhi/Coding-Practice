eac3c1a61186ae707f35fe2d5541b1e21aade182
rohitmanjhi96 <rohitmanjhibtc.com>
2025-12-15 17:42:51 +0530

    Fibonacci series problem

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
+        self.a, self.b = 0, 1
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.n:
+            raise StopIteration
+        value = self.a
+        self.a, self.b = self.b, self.a + self.b
+        self.count += 1
+        return value
+
+for x in Fibonacci(5):
+    print(x)  # 0 1 1 2 3
