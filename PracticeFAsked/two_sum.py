0425680db09df3e14f4aa67bf7f3adb631caa735
rohitmanjhi96 <rohitmanjhibtc.com>
2025-12-17 11:54:18 +0530

    two num sum problem solved

---
+++ PracticeFAsked/two_sum.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+print(two_sum([2, 7, 11, 15], 13))
