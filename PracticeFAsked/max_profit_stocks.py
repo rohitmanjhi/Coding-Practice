97d14eb35c0aeb60811d086e6dba04b9232a0dd3
rohitmanjhi96 <rohitmanjhibtc.com>
2025-12-17 12:02:14 +0530

    max profit same day selling a stocks

---
+++ PracticeFAsked/max_profit_stocks.py
@@ -0,0 +1,12 @@
+def max_profit(prices):
+    left, right = 0, 1
+    max_profit = 0
+    while right < len(prices):
+        if prices[right] > prices[left]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+print(max_profit([7,1,5,3,6,4]))
